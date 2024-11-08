1afe03b4cd1fccfac34833c3beb0100a872c6cb5
leela sai bhargav <leelasaibatthula05@gmail.com>
2024-11-08 15:46:23 +0530

    5 times

---
+++ saii.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class saii{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println("LEELA SAI BHARGAV");
+        }
+    }
+}
